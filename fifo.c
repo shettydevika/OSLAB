4809367d660be18703c031ac8b88726c79c98555
Devika Shetty <124135758+shettydevika@users.noreply.github.com>
2023-07-25 10:01:19 +0000

    commit

--- fifo.c
+++ fifo.c
@@ -13,10 +13,10 @@ int main()
     for(i=0;i<no;i++)
     frame[i]=-1;
     j=0;
-    printf("\tinput\tpage frames\n");
+    printf("\tinput\tpage\tframes\n");
     for(i=1;i<=n;i++)
     {
-        printf("%d\t\t",a[i]);
+        printf("%d\t",a[i]);
         avail=0;
         for(k=0;k<no;k++)
         if(frame[k]==a[i])
@@ -31,6 +31,25 @@ int main()
         }
         printf("\n");
     }
-    printf("Page Faiult is %d",fault);
+    printf("Page Fault is %d",fault);
     return 0;
 }
+
+/*OUTPUT:
+Enter the number of pages:
+7
+
+Enter the page number:
+1 3 0 3 5 6 3
+
+Enter the number of frames:
+3
+        input   page    frames
+1       1       -1      -1
+3       1       3       -1
+0       1       3       0
+3
+5       5       3       0
+6       5       6       0
+3       5       6       3
+Page Fault is 6 */
