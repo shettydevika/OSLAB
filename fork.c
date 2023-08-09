4d727e11c6c27936f59e0b9be65ffd284df21097
Devika Shetty <124135758+shettydevika@users.noreply.github.com>
2023-08-09 07:07:54 +0000

    commit

--- fork.c
+++ fork.c
@@ -1,28 +1,42 @@
 #include<stdio.h>
 #include<unistd.h>
 #include<sys/wait.h>
-int main(){
+int main()
+{
     pid_t pid;
     int status;
 
     pid=fork();
 
-    if(pid<0){
+    if(pid<0)
+    {
         printf("Error:fork() failed\n");
         return 1;
-    }else if(pid==0){
+    }
+    else if(pid==0)
+    {
         printf("This is the child process with PID:%d\n",getpid());
         printf("Parent process PID:%d\n",getpid());
 
         execlp("/bin/LS","LS",NULL);
         printf("This should not be printed if exec() is successful.\n");
         return 0;
-    }else{
+    }
+    else
+    {
         printf("This is the parent process with PID:%d\n",pid);
         printf("child process PID:%d\n",pid); 
         wait(&status);
         printf("child process exited with status %d\n",status);
-        return 0;
-    }
+        return 0;
+    }
 
 }
+/*
+Output:
+This is the parent process with PID:9275
+child process PID:9275
+This is the child process with PID:9275
+Parent process PID:9275
+This should not be printed if exec() is successful.
+child process exited with status 0*/
